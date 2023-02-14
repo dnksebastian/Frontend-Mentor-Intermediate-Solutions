2c795922101f2552fb583f061cc04f8b5308a3ca
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-14 16:21:36 +0100

    ip address tracker

--- ip-address-tracker-master/main.js
+++ ip-address-tracker-master/main.js
@@ -8,74 +8,81 @@ const locationValueElement = document.getElementById("location-value");
 const timezoneValueElement = document.getElementById("timezone-value");
 const ispValueElement = document.getElementById("isp-value");
 
-let latitude;
-let longitude;
-
 // Functions
+
+fetch("https://ipapi.co/json/")
+  .then((res) => res.json())
+  .then((data) => getMapData(data))
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+
+function getMapData(resData) {
+  const userIP = resData.ip;
+  const userLocation = `${resData.city}, ${resData.country_code} ${resData.postal}`;
+
+  if(resData.utc_offset) {
+    const splitUTC = (str, sub, pos) =>
+      `${str.slice(0, pos)}${sub}${str.slice(pos)}`;
+    const userUTC = splitUTC(resData.utc_offset, ":", 3);
+    const userTimezone = `UTC ${userUTC}`;
+    // This adds a colon to the UTC offset response string
+    timezoneValueElement.textContent = userTimezone;
+  }
+  else {
+    timezoneValueElement.textContent = `${resData.timezone}`
+  }
+
+  const userISP = resData.org;
+
+  ipValueElement.textContent = userIP;
+  locationValueElement.textContent = userLocation;
+  ispValueElement.textContent = userISP;
+
+  map.setView([resData.latitude, resData.longitude], 13);
+  mapMarker.setLatLng([resData.latitude, resData.longitude]);
+  // console.log(resData);
+}
 
 function submitForm(e) {
   e.preventDefault();
+
+  const fetchURL = `https://ipapi.co/${ipInputElement.value}/json/`;
+
+  fetch(fetchURL)
+  .then((res) => res.json())
+  .then((data) => getMapData(data))
+  .catch(function (error) {
+    console.log(error);
+  });
+
   console.log(ipInputElement.value);
+
   ipInputElement.value = "";
 }
-
-async function getUserIP() {
-  await fetch("https://ipapi.co/json/")
-    .then(function (response) {
-      response.json().then((jsonData) => {
-
-        const userIP = jsonData.ip;
-        const userLocation = `${jsonData.city}, ${jsonData.country_code} ${jsonData.postal}`;
-
-        const splitUTC = (str, sub, pos) => `${str.slice(0, pos)}${sub}${str.slice(pos)}`;
-        const userUTC = splitUTC(jsonData.utc_offset, ':', 3);
-        const userTimezone = `UTC ${userUTC}`;
-        // This adds a colon to the UTC offset response string
-
-        const userISP = jsonData.org;
-
-        const userLatitude = jsonData.latitude;
-        const userLongitude = jsonData.longitude;
-
-        ipValueElement.textContent = userIP;
-        locationValueElement.textContent = userLocation;
-        timezoneValueElement.textContent = userTimezone;
-        ispValueElement.textContent = userISP;
-
-        latitude = +userLatitude.toFixed(3);
-        longitude = +userLongitude.toFixed(3);
-
-        console.log(latitude, longitude);
-        
-        // console.log(jsonData);
-        // console.log(userLatitude, userLongitude);
-        // return [userLatitude, userLongitude];
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-getUserIP();
-
-// fetch('https://ipapi.co/8.8.8.8/json/')
-// .then(function(response) {
-//   response.json().then(jsonData => {
-//     console.log(jsonData);
-//   });
-// })
-// .catch(function(error) {
-//   console.log(error)
-// });
 
 // Event listeners
 
 formElement.addEventListener("submit", submitForm);
 
-const map = L.map("map").setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// Leaflet config
+
+// const map = L.map("map").setView([51.505, -0.09], 13);
+const map = L.map("map").setView([0, 0], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+const mapIcon = L.icon({
+  iconUrl: './images/icon-location.svg',
+  iconSize: [30, 40],
+  popupAnchor: [0, -5]
+});
+
+const mapMarker = L.marker([0,0], {icon: mapIcon}).addTo(map);
