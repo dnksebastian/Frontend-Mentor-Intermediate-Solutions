e1a2e1fe3228a75443a27b13bad51e2497b846f3
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-17 18:21:32 +0100

    easybank landing page

--- easybank-landing-page-master/main.js
+++ easybank-landing-page-master/main.js
@@ -1,1 +1,16 @@
-console.log(".");
+// DOM Elements
+const mobileMenuEl = document.getElementById('navwrap');
+const mobileMenuIconEl = document.getElementById('mob-icon');
+const backdropElement = document.getElementById('bdrop');
+
+// Functions
+
+const toggleMobileMenu = () => {
+    mobileMenuEl.classList.toggle('menu-active');
+    mobileMenuIconEl.classList.toggle('mobile-icon-active');
+    backdropElement.classList.toggle('backdrop-visible');
+};
+
+// Event listeners
+
+mobileMenuIconEl.addEventListener('click', toggleMobileMenu);
