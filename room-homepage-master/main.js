1b100dd7f237062ab33a6bc4f2d49631ed8947df
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-23 17:48:40 +0100

    room homepage

--- room-homepage-master/main.js
+++ room-homepage-master/main.js
@@ -1,1 +1,22 @@
-console.log(".");
+// DOM Elements & global variables
+const mobileMenuIconEl = document.getElementById("mob-menu-wrap");
+const mobileNavEl = document.getElementById("nav");
+
+let menuActive = false;
+
+// Functions
+const toggleMobileMenu = () => {
+  mobileMenuIconEl.classList.toggle("icon-active");
+
+  mobileNavEl.classList.toggle("menu-active");
+  mobileNavEl.classList.remove("hide-menu");
+
+  menuActive = !menuActive;
+
+  if (menuActive === false) {
+    mobileNavEl.classList.add("hide-menu");
+  }
+};
+
+// Event listeners
+mobileMenuIconEl.addEventListener("click", toggleMobileMenu);
