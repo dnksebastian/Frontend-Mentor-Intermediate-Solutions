daf5c27ea0379a2b87f286578bc9a9558c4f0c20
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-24 16:45:34 +0100

    room homepage

--- room-homepage-master/main.js
+++ room-homepage-master/main.js
@@ -1,8 +1,16 @@
 // DOM Elements & global variables
+
 const mobileMenuIconEl = document.getElementById("mob-menu-wrap");
 const mobileNavEl = document.getElementById("nav");
-
 let menuActive = false;
+
+
+const heroSlideEls = [... document.getElementsByClassName('hero-slide')];
+const controlLeftEl = document.getElementById('control-left');
+const controlRightEl = document.getElementById('control-right');
+
+let slideI = 1;
+changeSlide(slideI);
 
 // Functions
 const toggleMobileMenu = () => {
@@ -18,5 +26,23 @@ const toggleMobileMenu = () => {
   }
 };
 
+const showSlide = (n) => {
+  changeSlide(slideI += n)
+};
+
+function changeSlide(slideNum) {
+  if (slideNum > heroSlideEls.length) {slideI = 1}
+  if (slideNum < 1) {slideI = heroSlideEls.length}
+  
+  for (let i = 0; i < heroSlideEls.length; i++) {
+    heroSlideEls[i].classList.remove('slide-active');
+  }
+
+  heroSlideEls[slideI-1].classList.add('slide-active');
+};
+
+
 // Event listeners
 mobileMenuIconEl.addEventListener("click", toggleMobileMenu);
+controlLeftEl.addEventListener('click', showSlide.bind(null, -1));
+controlRightEl.addEventListener('click', showSlide.bind(null, 1));
