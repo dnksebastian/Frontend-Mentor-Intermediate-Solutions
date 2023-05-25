680aa14ebceafaf5240127641ad8bf5f509eca51
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-25 22:08:06 +0200

    calculator app

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -1,9 +1,18 @@
 // DOM Elements
+// Style elements
 const stylesLinkEl = document.getElementById("theme-ss");
 const themeTogglerEl = document.getElementById("theme-sw");
 const theme1RadioEl = document.getElementById("theme1");
 const theme2RadioEl = document.getElementById("theme2");
 const theme3RadioEl = document.getElementById("theme3");
+
+// Calculator elements
+const resetEl = document.querySelector('[data-val="reset"]');
+const delEl = document.querySelector('[data-val="del"]');
+const equalsEl = document.querySelector('[data-val="="]');
+
+const totalValEl = document.getElementById('total-value');
+const currentValEl = document.getElementById('current-value');
 
 const calculatorControlsEl = document.getElementById("calc-controls");
 
@@ -12,11 +21,6 @@ const calculatorControlsEl = document.getElementById("calc-controls");
 // Theme variables
 let userTheme = localStorage.getItem("userTheme");
 let chosenTheme = null;
-
-// Calculator variables
-let currentValue = null;
-let totalValue = null;
-
 
 // Functions
 
@@ -76,9 +80,127 @@ const switchTheme = (e) => {
   }
 };
 
+// Calculator Variables
+let currentVal = ''
+let previousVal = ''
+let resultVal = ''
+let currentOperation = ''
+let prevOperation = ''
+
 // Calculator functions
 
-//  ...
+const setCurrentVal = (e) => {
+    target = e.target
+
+    if(target.classList.contains('num')) {
+        let btnVal = target.dataset.val.toString()
+        
+        if(btnVal === '.' && currentVal.includes('.')) {
+          return
+        }
+
+        currentVal = currentVal + btnVal
+        currentValEl.textContent = currentVal
+    }
+}
+
+const setOperationSymbol = (e) => {
+  target = e.target
+
+  if(target.classList.contains('sign')) {
+    currentOperation = target.dataset.val.toString()
+  }
+
+}
+
+const calculate = (operation) => {
+
+  let current = parseFloat(currentVal)
+  let previous = parseFloat(previousVal)
+
+  switch(operation) {
+    
+    case "*":
+    resultVal = previous * current
+    break;
+
+    case "/":
+    resultVal = previous / current
+    break;
+
+    case "+":
+    resultVal = previous + current
+    break;
+
+    case "-":
+    resultVal = previous - current
+    break;
+
+    default: 
+    return
+  }
+
+}
+
+const getResult = (e) => {
+  target = e.target
+
+
+  if(target.classList.contains('sign')) {
+    
+    if(currentVal && !previousVal) {
+      previousVal = currentVal
+      prevOperation = currentOperation
+      totalValEl.textContent = `${previousVal}${prevOperation}`
+      currReset()
+      return
+    } else if(currentVal && previousVal) {
+      calculate(prevOperation)
+      currReset()
+      prevOperation = currentOperation
+      previousVal = resultVal
+      totalValEl.textContent = `${resultVal}${currentOperation}`
+    } else {
+      return
+    }
+
+  }
+
+  if(target.classList.contains('eq')) {
+    calculate(prevOperation)
+    currentValEl.textContent = `${resultVal}`
+    currentVal = ""
+    previousVal = ""
+    resultVal = ""
+    currentOperation = ""
+    prevOperation = ""
+    totalValEl.textContent = ""
+  }
+
+
+}
+
+
+const reset = () => {
+    currentVal = ""
+    previousVal = ""
+    resultVal = ""
+    currentOperation = ""
+    prevOperation = ""
+    currentValEl.textContent = ""
+    totalValEl.textContent = ""
+}
+
+const currReset = () => {
+  currentValEl.textContent = ""
+  currentVal = ""
+}
+
+const deleteInput = () => {
+  currentVal = currentVal.slice(0, -1)
+  currentValEl.textContent = currentVal
+}
+
 
 // Other functions
 
@@ -99,10 +221,18 @@ checkPreferredTheme();
 setPreferredTheme();
 
 // Event listeners
+themeTogglerEl.addEventListener("click", switchTheme);
+calculatorControlsEl.addEventListener("click", animateBtns);
+
 calculatorControlsEl.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-calculatorControlsEl.addEventListener("click", animateBtns);
+calculatorControlsEl.addEventListener('click', setCurrentVal)
+calculatorControlsEl.addEventListener('click', setOperationSymbol)
+// calculatorControlsEl.addEventListener('click', calculate)
+calculatorControlsEl.addEventListener('click', getResult)
 
-themeTogglerEl.addEventListener("click", switchTheme);
+resetEl.addEventListener('click', reset);
+delEl.addEventListener('click', deleteInput);
+// equalsEl.addEventListener('click', getResult);
