082555eedb03352fbabb23b51e1da69bf8f35729
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-26 17:13:53 +0200

    calculator app

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -11,8 +11,8 @@ const resetEl = document.querySelector('[data-val="reset"]');
 const delEl = document.querySelector('[data-val="del"]');
 const equalsEl = document.querySelector('[data-val="="]');
 
-const totalValEl = document.getElementById('total-value');
-const currentValEl = document.getElementById('current-value');
+const totalValEl = document.getElementById("total-value");
+const currentValEl = document.getElementById("current-value");
 
 const calculatorControlsEl = document.getElementById("calc-controls");
 
@@ -81,126 +81,123 @@ const switchTheme = (e) => {
 };
 
 // Calculator Variables
-let currentVal = ''
-let previousVal = ''
-let resultVal = ''
-let currentOperation = ''
-let prevOperation = ''
+let currentVal = "";
+let previousVal = "";
+let resultVal = "";
+let currentOperation = "";
+let prevOperation = "";
 
 // Calculator functions
 
 const setCurrentVal = (e) => {
-    target = e.target
+  target = e.target;
 
-    if(target.classList.contains('num')) {
-        let btnVal = target.dataset.val.toString()
-        
-        if(btnVal === '.' && currentVal.includes('.')) {
-          return
-        }
+  if (target.classList.contains("num")) {
+    let btnVal = target.dataset.val.toString();
 
-        currentVal = currentVal + btnVal
-        currentValEl.textContent = currentVal
+    if (btnVal === "." && currentVal.includes(".")) {
+      return;
     }
-}
+
+    currentVal = currentVal + btnVal;
+    currentValEl.textContent = currentVal;
+  }
+};
 
 const setOperationSymbol = (e) => {
-  target = e.target
+  target = e.target;
 
-  if(target.classList.contains('sign')) {
-    currentOperation = target.dataset.val.toString()
+  if (target.classList.contains("sign")) {
+    currentOperation = target.dataset.val.toString();
   }
-
-}
+};
 
 const calculate = (operation) => {
+  let current = parseFloat(currentVal);
+  let previous = parseFloat(previousVal);
 
-  let current = parseFloat(currentVal)
-  let previous = parseFloat(previousVal)
-
-  switch(operation) {
-    
+  switch (operation) {
     case "*":
-    resultVal = previous * current
-    break;
+      resultVal = +(previous * current).toPrecision(4);
+      break;
 
     case "/":
-    resultVal = previous / current
-    break;
+      resultVal = +(previous / current).toPrecision(4);
+      break;
 
     case "+":
-    resultVal = previous + current
-    break;
+      resultVal = +(previous + current).toPrecision(4);
+      break;
 
     case "-":
-    resultVal = previous - current
-    break;
+      resultVal = +(previous - current).toPrecision(4);
+      break;
 
-    default: 
-    return
+    default:
+      return;
   }
-
-}
+};
 
 const getResult = (e) => {
-  target = e.target
+  target = e.target;
 
-
-  if(target.classList.contains('sign')) {
-    
-    if(currentVal && !previousVal) {
-      previousVal = currentVal
-      prevOperation = currentOperation
-      totalValEl.textContent = `${previousVal}${prevOperation}`
-      currReset()
-      return
-    } else if(currentVal && previousVal) {
-      calculate(prevOperation)
-      currReset()
-      prevOperation = currentOperation
-      previousVal = resultVal
-      totalValEl.textContent = `${resultVal}${currentOperation}`
+  if (target.classList.contains("sign")) {
+    if (currentVal && !previousVal) {
+      previousVal = currentVal;
+      prevOperation = currentOperation;
+      totalValEl.textContent = `${previousVal}${prevOperation}`;
+      currReset();
+      return;
+    } else if (currentVal && previousVal) {
+      calculate(prevOperation);
+      currReset();
+      prevOperation = currentOperation;
+      previousVal = resultVal;
+      totalValEl.textContent = `${resultVal}${currentOperation}`;
+    } else if (resultVal && !currentVal) {
+      previousVal = resultVal;
+      prevOperation = currentOperation;
+      totalValEl.textContent = `${previousVal}${prevOperation}`;
+      currReset();
     } else {
-      return
+      return;
+    }
+  }
+
+  if (target.classList.contains("eq")) {
+    if (resultVal && !currentVal) {
+      return;
     }
 
+    calculate(prevOperation);
+    currentValEl.textContent = `${resultVal}`;
+    currentVal = "";
+    previousVal = "";
+    currentOperation = "";
+    prevOperation = "";
+    totalValEl.textContent = "";
   }
-
-  if(target.classList.contains('eq')) {
-    calculate(prevOperation)
-    currentValEl.textContent = `${resultVal}`
-    currentVal = ""
-    previousVal = ""
-    resultVal = ""
-    currentOperation = ""
-    prevOperation = ""
-    totalValEl.textContent = ""
-  }
-
-
-}
-
+};
 
 const reset = () => {
-    currentVal = ""
-    previousVal = ""
-    resultVal = ""
-    currentOperation = ""
-    prevOperation = ""
-    currentValEl.textContent = ""
-    totalValEl.textContent = ""
-}
+  currentVal = "";
+  previousVal = "";
+  resultVal = "";
+  currentOperation = "";
+  prevOperation = "";
+  currentValEl.textContent = "";
+  totalValEl.textContent = "";
+};
 
 const currReset = () => {
-  currentValEl.textContent = ""
-  currentVal = ""
-}
+  currentValEl.textContent = "";
+  currentVal = "";
+};
 
 const deleteInput = () => {
-  currentVal = currentVal.slice(0, -1)
-  currentValEl.textContent = currentVal
-}
-
+  currentVal = currentVal.slice(0, -1);
+  currentValEl.textContent = currentVal;
+};
 
 // Other functions
 
@@ -228,11 +225,10 @@ calculatorControlsEl.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-calculatorControlsEl.addEventListener('click', setCurrentVal)
-calculatorControlsEl.addEventListener('click', setOperationSymbol)
-// calculatorControlsEl.addEventListener('click', calculate)
-calculatorControlsEl.addEventListener('click', getResult)
+calculatorControlsEl.addEventListener("click", setCurrentVal);
+calculatorControlsEl.addEventListener("click", setOperationSymbol);
+calculatorControlsEl.addEventListener("click", getResult);
 
-resetEl.addEventListener('click', reset);
-delEl.addEventListener('click', deleteInput);
-// equalsEl.addEventListener('click', getResult);
+resetEl.addEventListener("click", reset);
+delEl.addEventListener("click", deleteInput);
+
