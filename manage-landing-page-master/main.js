b2aaa8fba5c10a3f8298ed6ff744321f71f2bbae
dnksebastian <sebastian.dulniawka@gmail.com>
2023-06-09 22:42:29 +0200

    manage landing page

--- manage-landing-page-master/main.js
+++ manage-landing-page-master/main.js
@@ -3,22 +3,22 @@ const mobileMenuIconEl = document.getElementById("mob-menu-wrap");
 const mobileMenuEl = document.getElementById("top-nav");
 const iconOpen = document.getElementById("hamburger-open");
 const iconClose = document.getElementById("hamburger-close");
-const backdropEl = document.getElementById('backdrop')
-const menuLinksEls = document.querySelectorAll('.nav-a');
+const backdropEl = document.getElementById("backdrop");
+const menuLinksEls = document.querySelectorAll(".nav-a");
 
 // Functions
 const toggleMobileMenu = () => {
   mobileMenuIconEl.classList.toggle("active");
   mobileMenuEl.classList.toggle("show");
-  document.querySelector('body').classList.toggle('backdrop-open')
-  
+  document.querySelector("body").classList.toggle("backdrop-open");
+
   if (mobileMenuEl.classList.contains("show")) {
     mobileMenuEl.style.display = "flex";
-    backdropEl.style.display = 'block'
+    backdropEl.style.display = "block";
   } else {
     setTimeout(() => {
       mobileMenuEl.style.display = "none";
-      backdropEl.style.display = 'none';
+      backdropEl.style.display = "none";
     }, 550);
   }
 
@@ -35,28 +35,29 @@ const toggleMobileMenu = () => {
 };
 
 const closeMenu = () => {
-    mobileMenuIconEl.classList.remove('active')
-    mobileMenuEl.classList.remove('show')
+  document.querySelector("body").classList.remove("backdrop-open");
+  mobileMenuIconEl.classList.remove("active");
+  mobileMenuEl.classList.remove("show");
 
-    mobileMenuEl.classList.remove('visible')
-    backdropEl.classList.remove('visible')
-    iconOpen.classList.remove('hidden')
-    iconClose.classList.remove('visible')
+  mobileMenuEl.classList.remove("visible");
+  backdropEl.classList.remove("visible");
+  iconOpen.classList.remove("hidden");
+  iconClose.classList.remove("visible");
 
-    mobileMenuEl.classList.add('hidden')
-    backdropEl.classList.add('hidden')
-    iconOpen.classList.add('visible')
-    iconClose.classList.add('hidden')
+  mobileMenuEl.classList.add("hidden");
+  backdropEl.classList.add("hidden");
+  iconOpen.classList.add("visible");
+  iconClose.classList.add("hidden");
 
-    setTimeout(() => {
-        mobileMenuEl.style.display = "none";
-        backdropEl.style.display = 'none';
-      }, 550);
-}
+  setTimeout(() => {
+    mobileMenuEl.style.display = "none";
+    backdropEl.style.display = "none";
+  }, 550);
+};
 
 // Event listeners
 mobileMenuIconEl.addEventListener("click", toggleMobileMenu);
-backdropEl.addEventListener('click', closeMenu)
+backdropEl.addEventListener("click", closeMenu);
 menuLinksEls.forEach((el) => {
-    el.addEventListener('click', closeMenu)
-})
+  el.addEventListener("click", closeMenu);
+});
