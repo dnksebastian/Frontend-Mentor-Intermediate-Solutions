b63d7e0df6246f41b85182ea9b050f4373c3a118
dnksebastian <sebastian.dulniawka@gmail.com>
2023-07-06 19:47:11 +0200

    job listings with filtering

--- static-job-listings-master/main.js
+++ static-job-listings-master/main.js
@@ -1,1 +1,12 @@
-console.log('.')
+const getJobOffers = async () => {
+    const response = await fetch('./data.json');
+    const jobData = await response.json()
+    return jobData
+  }
+
+  document.addEventListener('alpine:init', () => {
+    Alpine.store('jobsList', {
+      jobs: async () => await getJobOffers(),
+      filters: [],
+    })
+})
