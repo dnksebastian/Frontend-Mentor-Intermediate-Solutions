0bda501c26f01bb7aa8247be5ddf82196fb2aa29
dnksebastian <sebastian.dulniawka@gmail.com>
2023-07-09 17:47:52 +0200

    bookmark landing page

--- bookmark-landing-page-master/main.js
+++ bookmark-landing-page-master/main.js
@@ -1,1 +1,22 @@
-console.log(".")
+// DOM Elements
+const mobileMenuControlEl = document.getElementById('mobile-menu-control')
+const backdropEl = document.getElementById('backdrop')
+const topNavEl = document.getElementById('top-nav')
+const topLogoEl = document.getElementById('top-logo')
+const closeMenuIcon = document.getElementById('close-menu-icon')
+const hamburgerIcon = document.getElementById('hamburger-icon')
+
+// Functions
+
+const toggleMobileMenu = () => {
+    backdropEl.classList.toggle('active')
+    topNavEl.classList.toggle('active')
+    topLogoEl.classList.toggle('active')
+    hamburgerIcon.classList.toggle('active')
+    closeMenuIcon.classList.toggle('active')
+}
+
+
+// Event listeners
+
+mobileMenuControlEl.addEventListener('click', toggleMobileMenu)
