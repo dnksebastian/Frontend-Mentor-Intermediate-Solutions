a1ce60ceb58a8dfb6a69d1e216609a9711343ead
dnksebastian <sebastian.dulniawka@gmail.com>
2023-10-06 21:58:44 +0200

    ecommerce product page

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -1,1 +1,17 @@
-console.log('.');
+// Mobile menu
+document.addEventListener('alpine:init', () => {
+    Alpine.data('mobmenu', () => ({
+        open: false,
+        showClose: false,
+        showOpen: true,
+        showCart: false,
+        toggleMenu() {
+            this.open = !this.open
+            this.showClose = !this.showClose
+            this.showOpen = !this.showOpen
+        },
+        toggleCart() {
+            this.showCart = !this.showCart
+        }
+    }))
+})
