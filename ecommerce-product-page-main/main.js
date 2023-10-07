bd222de440990ab5334022369e7e1819ecc9dce8
dnksebastian <sebastian.dulniawka@gmail.com>
2023-10-07 20:36:48 +0200

    ecommerce product page

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -1,17 +1,38 @@
-// Mobile menu
 document.addEventListener('alpine:init', () => {
-    Alpine.data('mobmenu', () => ({
-        open: false,
+    Alpine.data('controls', () => ({
+        openMenu: false,
         showClose: false,
         showOpen: true,
+        currentSlide: 0,
         showCart: false,
+        cartItemsToAdd: 0,
+        currentCartItems: 0,
+        finalCartPrice: '',
         toggleMenu() {
-            this.open = !this.open
+            this.openMenu = !this.openMenu
             this.showClose = !this.showClose
             this.showOpen = !this.showOpen
         },
         toggleCart() {
             this.showCart = !this.showCart
+        },
+        addItemToAdd() {
+            this.cartItemsToAdd++
+        },
+        removeItemToAdd() {
+            if(this.cartItemsToAdd > 0) {
+                this.cartItemsToAdd--
+            }
+        },
+        updateCart() {
+            this.currentCartItems += this.cartItemsToAdd
+            this.cartItemsToAdd = 0
+            this.finalCartPrice = `$${(this.currentCartItems * 125).toFixed(2)}`
+        },
+        clearCart() {
+            this.currentCartItems = 0
+            this.cartItemsToAdd = 0
+            this.finalCartPrice = ''
         }
     }))
 })
