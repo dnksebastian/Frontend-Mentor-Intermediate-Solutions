b1583b3bf07cf57a619debe366fa2b056be1f7d7
dnksebastian <sebastian.dulniawka@gmail.com>
2023-10-08 16:57:17 +0200

    ecommerce product page

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -1,18 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('controls', () => ({
-        openMenu: false,
-        showClose: false,
-        showOpen: true,
         currentSlide: 0,
         showCart: false,
         cartItemsToAdd: 0,
         currentCartItems: 0,
         finalCartPrice: '',
-        toggleMenu() {
-            this.openMenu = !this.openMenu
-            this.showClose = !this.showClose
-            this.showOpen = !this.showOpen
-        },
         toggleCart() {
             this.showCart = !this.showCart
         },
@@ -33,6 +25,45 @@ document.addEventListener('alpine:init', () => {
             this.currentCartItems = 0
             this.cartItemsToAdd = 0
             this.finalCartPrice = ''
+        },
+        checkout() {
+            this.clearCart()
+            this.showCart = false;
         }
     }))
 })
+
+
+// Mobile menu
+
+const navMenuEl = document.getElementById('top-nav')
+const toggleMenuEl = document.querySelector('.menu-toggle-wrapper')
+const menuBackdropEl = document.querySelector('.backdrop');
+const iconOpenEl = document.querySelector('.icon-open');
+const iconCloseEl = document.querySelector('.icon-close');
+
+
+const toggleMobileMenu = () => {
+    navMenuEl.classList.toggle('menu-active');
+    menuBackdropEl.classList.toggle('backdrop-active')
+    menuBackdropEl.classList.toggle('backdrop-hidden');
+    iconOpenEl.classList.toggle('icon-visible');
+    iconOpenEl.classList.toggle('icon-hidden');
+    iconCloseEl.classList.toggle('icon-visible');
+    iconCloseEl.classList.toggle('icon-hidden');
+};
+
+const hideMenu = () => {
+    navMenuEl.classList.remove('menu-active');
+    menuBackdropEl.classList.remove('backdrop-active');
+    menuBackdropEl.classList.add('backdrop-hidden');
+    
+    iconOpenEl.classList.remove('icon-hidden')
+    iconOpenEl.classList.add('icon-visible')
+
+    iconCloseEl.classList.remove('icon-visible')
+    iconCloseEl.classList.add('icon-hidden')
+}
+
+toggleMenuEl.addEventListener('click', toggleMobileMenu);
+menuBackdropEl.addEventListener('click', hideMenu);
